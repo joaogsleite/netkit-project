649d189f1cf62aacfdd6d05c6873076a880aec91
joaaoleite <joaogsleite@gmail.com>
2016-05-17 00:22:31 +0100

    part2 dns,dhcp,zebra complete

---
+++ part2/start.sh
@@ -0,0 +1,18 @@
+
+
+if [ "$1" = "dns" ]; then
+	lstart sedeRouter sedePublicSwitch sedeEmailServer sedeHttpServer sedeAdminSwitch sedeRhPc leileiRouter leileiServer
+fi
+
+if [ "$1" = "ftp" ]; then
+	lstart sedeRouter sedePublicSwitch sedeEmailServer sedePrivateSwitch sedeFileServer sedeAdminSwitch sedeRhPc
+fi
+
+if [ "$1" = "ftp2" ]; then
+	lstart sedeRouter sedePublicSwitch sedeEmailServer sedePrivateSwitch sedeFileServer sedeAdminSwitch sedeRhPc leileiRouter leileiServer
+fi
+
+if [ "$1" = "lisboa" ]; then
+	lstart sedeRouter sedePublicSwitch sedeEmailServer leileiRouter leileiServer lisboaRouter lisboaAdminSwitch lisboaAdminArmPc
+fi
+
