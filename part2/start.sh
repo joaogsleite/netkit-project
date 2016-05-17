af6bf7362b708e5635079b047cd606204b9d9238
joaaoleite <joaogsleite@gmail.com>
2016-05-17 18:25:34 +0100

    el xumit do https

--- part2/start.sh
+++ part2/start.sh
@@ -20,6 +20,10 @@ if [ "$1" = "ftp2" ]; then
 	lstart sedeRouter sedePublicSwitch sedeEmailServer sedePrivateSwitch sedeFileServer sedeAdminSwitch sedeRhPc leileiRouter leileiServer
 fi
 
+if [ "$1" = "http" ]; then
+	lstart sedeRouter sedePublicSwitch sedeEmailServer sedeHttpServer sedeAdminSwitch sedeRhPc leileiRouter leileiServer
+fi
+
 if [ "$1" = "lisboa" ]; then
 	lstart sedeRouter sedePublicSwitch sedeEmailServer leileiRouter leileiServer lisboaRouter lisboaAdminSwitch lisboaAdminArmPc
 fi
